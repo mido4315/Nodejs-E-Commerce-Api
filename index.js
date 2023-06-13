194bdd927b76ac77c033986f47456cb21fe9b5dc
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-13 18:00:31 +0300

    use user model

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const authRouter = require('./routes/auth');
 const PORT = 6969;
+const DBC = "mongodb+srv://mido431:XBv6a32erHU4T1He@e-commerce-app.ejksezw.mongodb.net/";
+
+
 
 const app = express();
+
 app.use(authRouter);
+
+mongoose.connect(DBC).then(() => {
+    console.log("Connected");
+}).catch((e) => {
+    console.log(e);
+ })
+
 app.listen(PORT, "0.0.0.0",() => { 
     console.log(`Server is live on port : ${PORT}`);
 });
 
-app.get("/", (req, res) => {
-    res.send("hello people this is my first nodejs app");
-})
