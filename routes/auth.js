c88d07c562c80664922ed40f803f2fe82768dd70
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-13 23:31:00 +0300

    update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,26 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 const authRouter = express.Router();
 
-authRouter.get("/api/login", (req, res) => {
-  res.json({ msg: "mohamed mahmoud" });
-});
+authRouter.post("/api/signup", async (req, res) => {
+  try {
+    const { name, email, password, phoneNumber, address } = req.body;
 
-authRouter.get("/api/signup", async (req, res) => {
-  const { name, email, password, phoneNumber, address } = req.body;
+    const existingUSer = await User.findOne({ email });
 
-  const existingUSer = await User.find({ email });
-
-  if (existingUSer) {
-    return res.status(400).json({
-      msg: "The email address provided already exists.",
-    });
+    if (existingUSer) {
+      return res.status(400).json({
+        msg: "The email address provided already exists.",
+      });
+    }
+    const hPassword = await bcrypt.hash(password, 8);
+    let user = User({ name, email, password : hPassword, phoneNumber, address });
+    user = await user.save();
+    res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
-    let user = User({ name, email, password, phoneNumber, address });
-    user = user.save();
 });
 
 module.exports = authRouter;
