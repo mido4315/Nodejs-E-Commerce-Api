8b0f97f5c9a90f6f7c9092064adeeacdafd52019
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-13 15:52:19 +0300

    use express and add port

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello")
+const express = require("express");
+const PORT = 6969;
+
+const app = express();
+
+app.listen(PORT, "0.0.0.0", () => { 
+    console.log(`Server is live on port : ${PORT}`);
+});
