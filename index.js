b9eeba0e33e52264a41169428d6c34654bcc6caf
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-13 23:31:42 +0300

    register done

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const authRouter = require('./routes/auth');
+const mongoose = require("mongoose");
+const authRouter = require("./routes/auth");
 const PORT = 6969;
-const DBC = "mongodb+srv://mido431:XBv6a32erHU4T1He@e-commerce-app.ejksezw.mongodb.net/";
-
-
+const DBC =
+  "mongodb+srv://mido431:XBv6a32erHU4T1He@e-commerce-app.ejksezw.mongodb.net/";
 
 const app = express();
 
+app.use(express.json());
+
 app.use(authRouter);
 
-mongoose.connect(DBC).then(() => {
+mongoose
+  .connect(DBC)
+  .then(() => {
     console.log("Connected");
-}).catch((e) => {
+  })
+  .catch((e) => {
     console.log(e);
- })
+  });
 
-app.listen(PORT, "0.0.0.0",() => { 
-    console.log(`Server is live on port : ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is live on port : ${PORT}`);
 });
-
