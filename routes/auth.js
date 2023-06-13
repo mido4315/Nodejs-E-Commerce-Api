e54c2d551140e6f43f4c53ee1600499c35dba753
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-13 18:00:06 +0300

    create auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,24 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const User = require("../models/user");
 const authRouter = express.Router();
 
-authRouter.get("user", (req, res) => {
-    res.json({ msg: "mohamed mahmoud" });
-})
+authRouter.get("/api/login", (req, res) => {
+  res.json({ msg: "mohamed mahmoud" });
+});
+
+authRouter.get("/api/signup", async (req, res) => {
+  const { name, email, password, phoneNumber, address } = req.body;
+
+  const existingUSer = await User.find({ email });
+
+  if (existingUSer) {
+    return res.status(400).json({
+      msg: "The email address provided already exists.",
+    });
+  }
+    let user = User({ name, email, password, phoneNumber, address });
+    user = user.save();
+});
+
+module.exports = authRouter;
