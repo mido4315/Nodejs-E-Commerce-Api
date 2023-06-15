4865c44f7ab79572f8bc4272f7abaa17f44628a1
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-15 03:22:29 +0300

    update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,79 +1,76 @@
-const express = require("express");
-const User = require("../models/user");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const express = require('express');
+const User = require('../models/user');
+const myAuth = require('../components/my_auth')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const authRouter = express.Router();
 
-authRouter.post("/api/signup", async (req, res) => {
-  try {
-    const { name, email, password, phoneNumber, address } = req.body;
+authRouter.post('/api/signup', async (req, res) =>  {
+ try {
+    const {name , email, password} = req.body;
+    
+     const exisitingUser = await User.findOne({ email });
+     if (exisitingUser) {
+        return res.status(400).json({msg: "Email is already there, "});
+     }
 
-    const existingUSer = await User.findOne({ email });
+     const hPassword = await bcrypt.hash(password, 8);
 
-    if (existingUSer) {
-      return res.status(400).json({
-        msg: "The email address provided already exists.",
-      });
-    }
-    const hPassword = await bcrypt.hash(password, 8);
-    let user = User({ name, email, password: hPassword, phoneNumber, address });
-    user = await user.save();
+     let user = new User({email, password: hPassword, name}); 
 
-    const token = jwt.sign({ id: User._id }, "token");
-    res.json({
-      token,
-      ...user._doc,
-    });
-
-    //    res.json(user);
-  } catch (e) {
-    res.status(500).json({ error: e.message });
-  }
+     user = await user.save();
+     res.json(user);
+ } catch (e) {
+    res.status(500).json({error: e.message});
+ }
 });
 
-authRouter.post("/api/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const existingUSer = await User.findOne({ email });
-    if (!existingUSer) {
-      return res.status(400).json({
-        msg: "The email address provided doesn't exist",
-      });
-    }
-    const isMatched = bcrypt.compare(password, existingUSer.password);
-    if (!isMatched) {
-      return res.status(400).json({
-        msg: "Incorrect Password!",
-      });
-    }
-
-    const token = jwt.sign({ id: User._id }, "token");
-    res.json({
-      token,
-      ...existingUSer._doc,
-    });
-  } catch (e) {
-    res.status(500).json({ error: e.message });
-  }
-});
-
-authRouter.post("/isValidToken", async (req, res) => {
-  try {
-    const token = req.header("token");
-    if (!token) return res.status(502).json(false);
-    const verified = jwt.verify(token, "token");
-    if (!verified) {
-      return res.status(502).json(false);
-    } else {
-      const user = await User.findById(verified.id);
+authRouter.post('/api/signin', async (req, res) => {
+  try 
+  {
+      const {email, password} = req.body;
+      const user = await User.findOne({ email });
       if (!user) {
-        return res.status(502).json(false);
-      } else {
-        return res.json(true);
+         return res.status(400).json({msg: "User is not exist !"});
       }
-    }
-  } catch (e) {
-    res.status(500).json({ error: e.message });
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+         return res.status(400).json({msg: "Password is not correct !"});
+      }
+
+      const token = jwt.sign({id: user._id} , "passwordKey" );
+      res.json({ token, ...user._doc});
+  }
+  catch (e) {
+   res.status(500).json({error: e.message});
   }
 });
+
+authRouter.post('/isValidToken', async (req, res) => {
+   try {
+      const token = req.header('my-souq-auth-token');
+      if(!token) return res.json(false);
+      const verified = jwt.verify(token, "passwordKey" );
+      if (!verified) {
+         return res.json(false); 
+      } else  {
+          const user = await User.findById(verified.id);
+          if (!user) {
+            return res.json(false); 
+          } else {
+            return res.json(true); 
+          } 
+      }
+   } catch (e) {
+    res.status(500).json({error: e.message});
+   }
+ });
+
+authRouter.get('/', myAuth, async  (req, res) => {
+
+    const user = await User.findById(req.user);
+    res.json({ ...user._doc, token: req.token });
+});
+
 module.exports = authRouter;
