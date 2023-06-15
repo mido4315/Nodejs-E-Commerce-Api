99c3d55eb81411526a31a1f9842f12cf80d7a433
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-15 03:22:47 +0300

    create my auth

---
+++ components/my_auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const myAuth = async (req, res, next) => {
+    try {
+        const token = req.header('my-souq-auth-token');
+        if (!token) {
+            return res.status(401).json({msg: "Not atuthrized user"});
+        }
+
+        const verified = jwt.verify(token, "passwordKey");
+        if (!verified) {
+            return res.status(401).json({msg: "Token is not working, not allawed"});
+        }
+
+        req.user = verified.id; 
+        req.token = token;
+        next();
+    } catch (e) {
+      res.status(500).json({error: e.message});
+   }
+};
+
+module.exports =  myAuth;
